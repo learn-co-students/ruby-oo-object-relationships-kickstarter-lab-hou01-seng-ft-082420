e84cace69917f48d2902e24d9a2460de48dfa5a4
stephenandersondev <stephenandersondev@gmail.com>
2020-08-28 14:13:17 -0500

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project,self)
+    end
+
+    def backed_projects
+        ((ProjectBacker.all.select {|pb| pb.backer == self}).collect{|p| p.project})
+    end
+
+end
